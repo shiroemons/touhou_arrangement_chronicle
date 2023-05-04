9cec9dec7a257aba4bf06deb1e52b2cc24403c63
shiroemons <shiroemons@gmail.com>
2023-05-04 10:44:16 +0900

    ユニーク制約を追加

--- internal/entity/tag.go
+++ internal/entity/tag.go
@@ -10,7 +10,7 @@ type Tag struct {
 	bun.BaseModel `bun:"table:tags,alias:tag"`
 
 	ID        string    `bun:",pk,default:xid()"`
-	Name      string    `bun:"name,nullzero,notnull"`
+	Name      string    `bun:"name,nullzero,notnull,unique"`
 	TagType   string    `bun:"tag_type,nullzero,notnull,default:'unknown'::tag_type"`
 	CreatedAt time.Time `bun:"created_at,notnull,default:current_timestamp"`
 	UpdatedAt time.Time `bun:"updated_at,notnull,default:current_timestamp"`
