22ca380227b2aa0d4c4021df2c3c4bbb11536fe9
shiroemons <shiroemons@gmail.com>
2024-04-29 14:01:00 +0900

    Update example

--- frontend/app/routes/example.tsx
+++ frontend/app/routes/example.tsx
@@ -7,5 +7,16 @@ export default function Example() {
     return null;
   }
 
-  return <div>Hello, {user.firstName} welcome to Clerk</div>;
+  return (
+    <div>
+      Hello, {user.firstName} welcome to Clerk
+      <p>id: {user.id}</p>
+      <p>external id: {user.externalId}</p>
+      <p>email: {user.primaryEmailAddress?.emailAddress}</p>
+      <p>username: {user.username}</p>
+      <p>full name: {user.fullName}</p>
+      <p>first name: {user.firstName}</p>
+      <p>last name: {user.lastName}</p>
+    </div>
+  );
 }
