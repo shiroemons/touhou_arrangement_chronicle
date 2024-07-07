9a7376610dbb8c339d7c92f9d96b835760e0f6f1
shiroemons <shiroemons@gmail.com>
2024-07-07 18:17:28 +0900

    indexページを更新

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,16 +1,26 @@
-import { SignedIn, SignedOut } from "@clerk/remix";
 import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import {
+  Box,
+  Button,
+  HStack,
+  Heading,
+  Stat,
+  Text,
+  VStack,
+  Wrap,
+} from "@yamada-ui/react";
 import { countDistinct } from "drizzle-orm";
-import Stat from "~/components/Stat";
+
+import { MusicDialog } from "~/components/MusicDialog";
 import { db } from "~/services/db.server";
 import { albums, artists, circles, songs } from "~/services/schema.server";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "東方編曲録　〜 Arrangement Chronicle" },
+    { name: "description", content: "東方Projectの東方アレンジのデータベース" },
   ];
 };
 
@@ -48,21 +58,66 @@ export default function Index() {
     useLoaderData() as LoaderData;
 
   return (
-    <div>
-      <h1 className="text-2xl">Index Route</h1>
-      <Stat
-        songCount={songCount || 0}
-        albumCount={albumCount || 0}
-        artistCount={artistCount || 0}
-        circleCount={circleCount || 0}
-      />
-      <p>There are {songCount} song(s) in the database</p>
-      <SignedIn>
-        <p>You are signed in!</p>
-      </SignedIn>
-      <SignedOut>
-        <p>You are signed out</p>
-      </SignedOut>
-    </div>
+    <Box
+      bgSize="cover"
+      bgPosition="center"
+      height="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      textAlign="center"
+    >
+      <VStack spacing="4" maxW="800px">
+        <Heading as="h1" size="3xl" textAlign="center">
+          東方編曲録 〜 Arrangement Chronicle
+        </Heading>
+        <Text fontSize="2xl" textAlign="center">
+          幻想郷の音楽をここに集めて
+        </Text>
+        <Text
+          fontSize="lg"
+          maxW="3xl"
+          textAlign="center"
+          lineHeight="1.6"
+          mt={4}
+          px={4}
+        >
+          東方Projectのファンによる、ファンのためのアレンジ楽曲データベースです。原曲別、サークル別、アーティスト別、イベント別に楽曲を検索でき、原曲の使用状況もグラフで確認できます。
+        </Text>
+        <Text
+          fontSize="lg"
+          maxW="3xl"
+          textAlign="center"
+          lineHeight="1.6"
+          mt={4}
+          px={4}
+        >
+          幻想郷の魅力的な音楽の世界をお楽しみください。
+        </Text>
+        <HStack spacing={4}>
+          <Button
+            colorScheme="red"
+            size="lg"
+            boxShadow="lg"
+            _hover={{ boxShadow: "xl" }}
+          >
+            今すぐ検索
+          </Button>
+          <MusicDialog />
+        </HStack>
+        <Box mt="8" width="100%">
+          <Wrap gap="md">
+            <Stat label="楽曲登録数" number={songCount} width="180px" />
+            <Stat label="アルバム登録数" number={albumCount} width="180px" />
+            <Stat
+              label="アーティスト登録数"
+              number={artistCount}
+              width="180px"
+            />
+            <Stat label="サークル登録数" number={circleCount} width="180px" />
+          </Wrap>
+        </Box>
+      </VStack>
+    </Box>
   );
 }
